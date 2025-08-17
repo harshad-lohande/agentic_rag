6f79e6497475f1ebe61f7c8582b3645de24dcb3f
Harshad Lohande <lohande.harshad@gmail.com>
2025-08-17 09:31:17 +0530

    Loads the logging config from logging.ini file.

---
+++ app/logging_config.py
@@ -0,0 +1,13 @@
+# app/logging_config.py
+
+import logging.config
+
+def setup_logging():
+    """
+    Loads the logging configuration from the logging.ini file.
+    This should be called at the start of any application entry point.
+    """
+    logging.config.fileConfig('logging.ini', disable_existing_loggers=False)
+
+# Get the logger instance to be imported by other modules
+logger = logging.getLogger("agentic_rag")
